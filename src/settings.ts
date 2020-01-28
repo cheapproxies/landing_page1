30d73e11f011f9c5030ed5dd14af2832138f4c4f
cheapproxies <59569096+cheapproxies@users.noreply.github.com>
2020-01-28 19:24:54 +0100

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -4,27 +4,27 @@
  This is the link to your domain...it redirects the user to your myaccount page.
  It is used on the site on buttons like: Buy now, Order now, Members, Buy HTTP proxies, etc.
 */
-export const MEMBERS_URL = "https://members.url.goes.here";
+export const MEMBERS_URL = "https://myaccount.cheapproxies.net";
 
 /*
  Your site name - this appears in the footer
 */
-export const SITE_NAME = "Site name goes here";
+export const SITE_NAME = "cheapproxies.net";
 
 /*
  The name of you site that appears in Terms of service section - it can be you domain, your company name, etc.
 */
-export const TOS_SITE_NAME = 'whatever-you-need';
+export const TOS_SITE_NAME = 'cheapproxies.net';
 
 /*
  The date when the terms of service where last updated.
 */
-export const TOS_LAST_UPDATE = 'Apr-27-2018';
+export const TOS_LAST_UPDATE = 'Jan-2020';
 
 /*
  This is the link of your chat widget - it can be found on your tawk to account
 */
-export const TAWK_TO_WIDGET_URL = 'https://embed.tawk.to/5b61937de21878736ba285c5/default';
+export const TAWK_TO_WIDGET_URL = 'https://tawk.to/chat/5e1761917e39ea1242a3da37/default';
 
 /*
  This is the link to your backend services. It is used to get the data for prices and datacenters. Normally, you shouldn't touch this one
@@ -36,4 +36,4 @@ export const BACKEND_URL = 'https://backend.changemyip.com';
 */
 export const SEO_DESCRIPTION = 'Fully anonymous private proxies (shared or dedicated)';
 // The keywords need to be separated by comma
-export const SEO_KEYWORDS = 'proxy, socks, shared proxies, dedicated proxies, residential proxies, socksv5';
+export const SEO_KEYWORDS = 'proxy, socks, shared proxies, dedicated proxies, residential proxies, socksv5, proxies';
